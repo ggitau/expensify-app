a2ae709bd413a7da31292b06896f467ed1e442a3
geogit <com00204@gmail.com>
2018-01-19 16:58:43 +0300

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,14 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.dispatch(addExpense({description:'Water bill',amount:1000,createdAt:1000}));
-store.dispatch(addExpense({description:'Gas bill',amount:500,createdAt:1200}));
-store.dispatch(addExpense({description:'Water bill',amount:1600,createdAt:1100}));
-
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses,state.filters));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
