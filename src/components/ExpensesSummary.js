1e628047cb40efc97faad7a9e699682677c4df19
geogit <com00204@gmail.com>
2018-01-21 16:26:00 +0300

    Setup filters for expenses summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import numeral from 'numeral';
 import {connect} from 'react-redux';
 import totalExpenses from '../selectors/expenses-total';
+import visibleExpenses from '../selectors/expenses';
 
 export class ExpensesSummary extends React.Component{
     render(){
@@ -22,8 +23,8 @@ export class ExpensesSummary extends React.Component{
 }
 const mapStateToProps = (state) => {
     return {
-        expensesTotal:numeral(totalExpenses(state.expenses)/100).format('$0,0.00'),
-        expensesCount:state.expenses.length
+        expensesTotal:numeral(totalExpenses(visibleExpenses(state.expenses,state.filters))/100).format('$0,0.00'),
+        expensesCount:visibleExpenses(state.expenses,state.filters).length
     };
 };
 export default connect(mapStateToProps)(ExpensesSummary);
